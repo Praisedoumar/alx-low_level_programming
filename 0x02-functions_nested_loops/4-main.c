f7ff191cc7aa8081ff5b76869f34e9457f872968
Praisedoumar <Adigunpraiseoluwafikayo@gmail.com>
2022-10-18 16:27:17 -0700

    c pro

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -1,20 +1,14 @@
-#include "main.h"                                                                                                                                               
+#include "main.h"
 
-/**                                                                                                                                                             
- * _isalpha - check for alphabets                                                                                                                               
- * @c: character to be checked                                                                                                                                  
- *                                                                                                                                                              
- * Return: 1 if character is a letter,0 otherwise                                                                                                               
- */                                                                                                                                                             
-int _isalpha(int c)                                                                                                                                           
-{                                                                                                                                                               
-
-        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))                                                                                                   
-
-                return (1);                                                                                                                                     
-
-        else                                                                                                                                                    
-
-                return (0);                                                                                                                                     
-
-}  
+/**
+ *  _isalpha - checks for alphabets
+ *  @c: character to be check
+ *  Return: 1 if character is a letter, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
