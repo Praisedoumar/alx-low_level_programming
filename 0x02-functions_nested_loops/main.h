78d3951683d6827f088b4c14da8171773e04cf65
Praisedoumar <Adigunpraiseoluwafikayo@gmail.com>
2022-10-18 13:24:26 -0700

    c pro

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,7 +10,7 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int; int);
+int add(int, int);
 void print_to_98(int n);
 int _putchar(char c);
 
