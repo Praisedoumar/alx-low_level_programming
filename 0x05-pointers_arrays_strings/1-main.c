254f52c083f669699d1df323ba142a21b66130c7
Praisedoumar <Adigunpraiseoluwafikayo@gmail.com>
2022-10-24 12:48:04 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/1-main.c
@@ -0,0 +1,15 @@
+#incude "main.h"
+
+/**
+ * swap_int - swaps theh values of two integers
+ * @a: int a
+ * @b: int b
+ */
+void swap_int(int *a, int *b)
+{
+	int q;
+
+	q = *a;
+	*a = *b;
+	*b = q;
+}
